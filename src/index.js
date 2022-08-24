878fd9f22ef7c8e7a454b98b3ccde9d443d42203
Aleksei Ovchinnikov <amgstrider@gmail.com>
2022-08-24 23:04:20 +0400

    feat: complete prime functional

--- src/index.js
+++ src/index.js
@@ -23,6 +23,19 @@ function removeActiveClass(array) {
     })
 }
 
+function shuffleArray(array) {
+    let shuffledArray = [];
+    let tempArray = [];
+
+    tempArray.push(...array);
+
+    for (let i = 0; i < array.length; i++) {
+        shuffledArray.push(tempArray.splice(getRandomNumber(tempArray.length), 1)[0]);
+    }
+
+    return shuffledArray;
+}
+
 ancientsArray.forEach((e, i) => {
     e.style.backgroundImage = `url(${ancients[i].cardFace})`;
 })
@@ -53,16 +66,12 @@ function getRandomNumber(max) {
 
 function getCards(ancient = 'azathoth', difficulty = 'normal') {
     let currentAncient = ancients.filter(ancients => ancients.id === ancient)[0];
-    console.log(currentAncient);
-
     let blueCardsFullArray = [];
-    blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue');
-
     let brownCardsFullArray = [];
-    brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown');
-
     let greenCardsFullArray = [];
-    greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green');
+    let blueCardsArray = [];
+    let brownCardsArray = [];
+    let greenCardsArray = [];
 
     let blueCardsNeeded = currentAncient.firstStage.blueCards + currentAncient.secondStage.blueCards + currentAncient.thirdStage.blueCards;
 
@@ -70,61 +79,136 @@ function getCards(ancient = 'azathoth', difficulty = 'normal') {
 
     let greenCardsNeeded = currentAncient.firstStage.greenCards + currentAncient.secondStage.greenCards + currentAncient.thirdStage.greenCards;
 
-    let blueCardsArray = [];
-    let brownCardsArray = [];
-    let greenCardsArray = [];
 
-    if (difficulty === 'normal') {
+    if (difficulty === 'veryEasy') {
+        blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty === 'easy');
 
-        for (let i = 0; i < blueCardsNeeded; i++) {
-            let number = getRandomNumber(blueCardsFullArray.length);
-            let cardCheck = blueCardsFullArray[number];
+        if (blueCardsFullArray.length < blueCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty === 'normal'));
 
-            if (!blueCardsArray) {
-                blueCardsArray.push(blueCardsFullArray[number]);
-            } else if (!(blueCardsArray.filter(v => v === cardCheck)[0])) {
-                blueCardsArray.push(blueCardsFullArray[number]);
-            } else {
-                while ((blueCardsArray.filter(v => v === cardCheck)[0])) {
-                    number = getRandomNumber(blueCardsFullArray.length);
-                    cardCheck = blueCardsFullArray[number];
-                }
-                blueCardsArray.push(blueCardsFullArray[number]);
+            while (blueCardsFullArray.length < blueCardsNeeded) {
+                blueCardsFullArray.push(normalCardsArray.shift());
             }
         }
 
-        for (let i = 0; i < brownCardsNeeded; i++) {
-            let number = getRandomNumber(brownCardsFullArray.length);
-            let cardCheck = brownCardsFullArray[number];
+        brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty === 'easy');
 
-            if (!brownCardsArray) {
-                brownCardsArray.push(brownCardsFullArray[number]);
-            } else if (!(brownCardsArray.filter(v => v === cardCheck)[0])) {
-                brownCardsArray.push(brownCardsFullArray[number]);
-            } else {
-                while (brownCardsArray.filter(v => v === cardCheck)[0]) {
-                    number = getRandomNumber(brownCardsFullArray.length);
-                    cardCheck = brownCardsFullArray[number];
-                }
-                brownCardsArray.push(brownCardsFullArray[number]);
+        if (brownCardsFullArray.length < brownCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty === 'normal'));
+
+            while (brownCardsFullArray.length < brownCardsNeeded) {
+                brownCardsFullArray.push(normalCardsArray.shift());
             }
         }
 
-        for (let i = 0; i < greenCardsNeeded; i++) {
-            let number = getRandomNumber(greenCardsFullArray.length);
-            let cardCheck = greenCardsFullArray[number];
+        greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty === 'easy');
 
-            if (!greenCardsArray) {
-                greenCardsArray.push(greenCardsFullArray[number]);
-            } else if (!(greenCardsArray.filter(v => v === cardCheck)[0])) {
-                greenCardsArray.push(greenCardsFullArray[number]);
-            } else {
-                while (greenCardsArray.filter(v => v === cardCheck)[0]) {
-                    number = getRandomNumber(greenCardsFullArray.length);
-                    cardCheck = greenCardsFullArray[number];
-                }
-                greenCardsArray.push(greenCardsFullArray[number]);
+        if (greenCardsFullArray.length < greenCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty === 'normal'));
+
+            while (greenCardsFullArray.length < greenCardsNeeded) {
+                greenCardsFullArray.push(normalCardsArray.shift());
             }
+        }
+
+    } else if (difficulty === 'easy') {
+        blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty !== 'hard');
+
+        brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty !== 'hard');
+
+        greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty !== 'hard');
+    } else if (difficulty === 'normal') {
+        blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue');
+
+        brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown');
+
+        greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green');
+    } else if (difficulty === 'hard') {
+        blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty !== 'easy');
+
+        brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty !== 'easy');
+
+        greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty !== 'easy');
+    } else if (difficulty === 'veryHard') {
+        blueCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty === 'hard');
+
+        if (blueCardsFullArray.length < blueCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'blue' && mythicCards.difficulty === 'normal'));
+
+            while (blueCardsFullArray.length < blueCardsNeeded) {
+                blueCardsFullArray.push(normalCardsArray.shift());
+            }
+        }
+
+        brownCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty === 'hard');
+
+        if (brownCardsFullArray.length < brownCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'brown' && mythicCards.difficulty === 'normal'));
+
+            while (brownCardsFullArray.length < brownCardsNeeded) {
+                brownCardsFullArray.push(normalCardsArray.shift());
+            }
+        }
+
+        greenCardsFullArray = mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty === 'hard');
+
+        if (greenCardsFullArray.length < greenCardsNeeded) {
+            let normalCardsArray = shuffleArray(mythicCards.filter(mythicCards => mythicCards.color === 'green' && mythicCards.difficulty === 'normal'));
+
+            while (greenCardsFullArray.length < greenCardsNeeded) {
+                greenCardsFullArray.push(normalCardsArray.shift());
+            }
+        }
+    }
+
+    for (let i = 0; i < blueCardsNeeded; i++) {
+        let number = getRandomNumber(blueCardsFullArray.length);
+        let cardCheck = blueCardsFullArray[number];
+
+        if (!blueCardsArray) {
+            blueCardsArray.push(blueCardsFullArray[number]);
+        } else if (!(blueCardsArray.filter(v => v === cardCheck)[0])) {
+            blueCardsArray.push(blueCardsFullArray[number]);
+        } else {
+            while ((blueCardsArray.filter(v => v === cardCheck)[0])) {
+                number = getRandomNumber(blueCardsFullArray.length);
+                cardCheck = blueCardsFullArray[number];
+            }
+            blueCardsArray.push(blueCardsFullArray[number]);
+        }
+    }
+
+    for (let i = 0; i < brownCardsNeeded; i++) {
+        let number = getRandomNumber(brownCardsFullArray.length);
+        let cardCheck = brownCardsFullArray[number];
+
+        if (!brownCardsArray) {
+            brownCardsArray.push(brownCardsFullArray[number]);
+        } else if (!(brownCardsArray.filter(v => v === cardCheck)[0])) {
+            brownCardsArray.push(brownCardsFullArray[number]);
+        } else {
+            while (brownCardsArray.filter(v => v === cardCheck)[0]) {
+                number = getRandomNumber(brownCardsFullArray.length);
+                cardCheck = brownCardsFullArray[number];
+            }
+            brownCardsArray.push(brownCardsFullArray[number]);
+        }
+    }
+
+    for (let i = 0; i < greenCardsNeeded; i++) {
+        let number = getRandomNumber(greenCardsFullArray.length);
+        let cardCheck = greenCardsFullArray[number];
+
+        if (!greenCardsArray) {
+            greenCardsArray.push(greenCardsFullArray[number]);
+        } else if (!(greenCardsArray.filter(v => v === cardCheck)[0])) {
+            greenCardsArray.push(greenCardsFullArray[number]);
+        } else {
+            while (greenCardsArray.filter(v => v === cardCheck)[0]) {
+                number = getRandomNumber(greenCardsFullArray.length);
+                cardCheck = greenCardsFullArray[number];
+            }
+            greenCardsArray.push(greenCardsFullArray[number]);
         }
     }
 
@@ -234,7 +318,11 @@ coverImage.addEventListener('click', () => {
         cardImage.style.backgroundImage = `url(${secondStage.shift().cardFace})`;
     } else if (thirdStage.length) {
         cardImage.style.backgroundImage = `url(${thirdStage.shift().cardFace})`;
+        if (thirdStage.length === 0) {
+            coverImage.classList.add('hide');
+        }
     }
+
 
     showCardsInDeck();
 })
@@ -245,7 +333,9 @@ startButton.addEventListener('click', () => {
         secondStage = [];
         thirdStage = [];
         coverImage.style.backgroundImage = "url('./assets/img/mythicCardBackground.png')"
-        getCards(activeAncient);
+        cardImage.style.backgroundImage = "url('')"
+        coverImage.classList.remove('hide');
+        getCards(activeAncient, activeDifficulty);
     }
 })
 
